4257d855014ad6a83dd1a43f371f34c8391714ed
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-07-04 16:22:42 +0300

    form

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 import css from './Form.module.css';
 import PropTypes from 'prop-types';
 
-const Contacts = () => {
-  return <div></div>;
+const Contacts = ({ titleName, contacts, ids }) => {
+  return (
+    <div>
+      <h2>{titleName}</h2>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.id}>
+            {contact.name}: {contact.number}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-Contacts.propTypes = {};
+Contacts.propTypes = {
+  titleName: PropTypes.string.isRequired,
+  //   ids: PropTypes.string.isRequired,
+  //   contacts: PropTypes.string.isRequired,
+};
 
 export default Contacts;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import css from './Form.module.css';
 import { nanoid } from 'nanoid';
+import Contacts from './Contacts';
 
 import PropTypes from 'prop-types';
 
@@ -10,15 +11,42 @@ class Form extends React.Component {
     this.state = {
       contacts: [],
       name: '',
+      number: '',
     };
   }
   nameLabelID = nanoid();
   numberLabelID = nanoid();
 
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleNumberChange = event => {
+    this.setState({ number: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { name, number, contacts } = this.state;
+    if (name && number) {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      this.setState({
+        contacts: [...contacts, newContact],
+        name: '',
+        number: '',
+      });
+    }
+  };
+
   render() {
     const {} = this.props;
+    const { contacts } = this.state;
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor={this.nameLabelID}>Name</label>
         <input
           className={css.userName}
@@ -27,6 +55,8 @@ class Form extends React.Component {
           pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          value={this.state.name}
+          onChange={this.handleNameChange}
         />
         <label htmlFor={this.numberLabelID}>Number</label>
         <input
@@ -35,8 +65,12 @@ class Form extends React.Component {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          value={this.state.number}
+          onChange={this.handleNumberChange}
         />
         <button type="submit">Submit</button>
+
+        <Contacts titleName={'Contacts'} contacts={contacts} />
       </form>
     );
   }
